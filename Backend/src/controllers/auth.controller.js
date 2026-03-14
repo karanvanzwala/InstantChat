5d2adf1263c5258bd7b34fab7ba297423214381a
karanvanzwala <karan.vanzwala@gmail.com>
2026-03-14 15:51:09 +0530

    add protected routes

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { sendWelcomeEmail } from "../emails/emailHandlers.js";
+import cloudinary from "../lib/cloudinary.js";
 import { ENV } from "../lib/env.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/User.js";
@@ -102,5 +103,24 @@ export const logout = async (_, res) => {
 }
 
 export const updateProfile = async (req, res) => {
+
+    try {
+        const { profilePic } = req.body
+        if (!profilePic) return res.status(400).json({ message: "Profile picture is required" })
+
+        const userId = req.user._id
+
+        const uploadResponse = await cloudinary.uploader.upload(profilePic)
+
+        const updateUser = await User.findByIdAndUpdate(userId, { profilePic: uploadResponse.secure_url }, {
+            new: true
+        })
+        res.status(200).json(updateUser)
+
+    } catch (error) {
+        console.log("error in update profile", error)
+        res.status(500).json({ message: "Internal server error" })
+
+    }
 
 }
--- Backend/src/routes/auth.route.js
+++ Backend/src/routes/auth.route.js
@@ -1,10 +1,21 @@
 import express from "express";
 import { signup, login, logout, updateProfile } from "../controllers/auth.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js";
+import { arcjetProtection } from "../middleware/arcjet.middleware.js";
 
 const router = express.Router()
 
+router.use(arcjetProtection);
+
+
 router.post("/singup", signup)
+
+
+// router.get("/test", (req, res) => {
+//     console.log("666776")
+//     res.send("hello i am from message side")
+
+// })
 
 router.post("/login", login)
 router.post("/logout", logout)
